b754cd5edf65a2d804f790f5dbe441046b26385c
Wang Gengyu <posuer000@gmail.com>
2020-03-22 15:16:12 -0400

    imho cmv data

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # argument-mining
+
+## finetune with IMHO
+```bash
+export TASK_NAME=IMHO
+
+python run_argmining.py \
+    --model_type bert \
+    --model_name_or_path bert-base-uncased \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_lower_case \
+    --data_dir ./$TASK_NAME \
+    --max_seq_length 128 \
+    --per_gpu_eval_batch_size=8   \
+    --per_gpu_train_batch_size=8   \
+    --learning_rate 2e-5 \
+    --num_train_epochs 3.0 \
+    --output_dir /tmp/$TASK_NAME/
+```
+## finetune with ChangeMyView
+```bash
+export TASK_NAME=CMV
+
+python run_argmining.py \
+    --model_type bert \
+    --model_name_or_path ./pytorch_model.bin \
+    --task_name $TASK_NAME \
+    --do_train \
+    --do_lower_case \
+    --data_dir ./$TASK_NAME \
+    --max_seq_length 128 \
+    --per_gpu_eval_batch_size=8   \
+    --per_gpu_train_batch_size=8   \
+    --learning_rate 2e-5 \
+    --num_train_epochs 3.0 \
+    --output_dir /tmp/$TASK_NAME/
+```
---
+++ data_loader.py
@@ -0,0 +1,258 @@
+""" IMHO processors and helpers """
+
+import logging
+import os
+
+import tensorflow as tf
+from transformers import DataProcessor, InputExample, InputFeatures
+
+logger = logging.getLogger(__name__)
+is_tf_available = True
+
+def convert_examples_to_features(
+    examples,
+    tokenizer,
+    max_length=512,
+    task=None,
+    label_list=None,
+    output_mode=None,
+    pad_on_left=False,
+    pad_token=0,
+    pad_token_segment_id=0,
+    mask_padding_with_zero=True,
+):
+    """
+    Loads a data file into a list of ``InputFeatures``
+
+    Args:
+        examples: List of ``InputExamples`` or ``tf.data.Dataset`` containing the examples.
+        tokenizer: Instance of a tokenizer that will tokenize the examples
+        max_length: Maximum example length
+        task: GLUE task
+        label_list: List of labels. Can be obtained from the processor using the ``processor.get_labels()`` method
+        output_mode: String indicating the output mode. Either ``regression`` or ``classification``
+        pad_on_left: If set to ``True``, the examples will be padded on the left rather than on the right (default)
+        pad_token: Padding token
+        pad_token_segment_id: The segment ID for the padding token (It is usually 0, but can vary such as for XLNet where it is 4)
+        mask_padding_with_zero: If set to ``True``, the attention mask will be filled by ``1`` for actual values
+            and by ``0`` for padded values. If set to ``False``, inverts it (``1`` for padded values, ``0`` for
+            actual values)
+
+    Returns:
+        If the ``examples`` input is a ``tf.data.Dataset``, will return a ``tf.data.Dataset``
+        containing the task-specific features. If the input is a list of ``InputExamples``, will return
+        a list of task-specific ``InputFeatures`` which can be fed to the model.
+
+    """
+    is_tf_dataset = False
+    if is_tf_available and isinstance(examples, tf.data.Dataset):
+        is_tf_dataset = True
+
+    if task is not None:
+        processor = processors[task]()
+        if label_list is None:
+            label_list = processor.get_labels()
+            logger.info("Using label list %s for task %s" % (label_list, task))
+        if output_mode is None:
+            output_mode = output_modes[task]
+            logger.info("Using output mode %s for task %s" % (output_mode, task))
+
+    label_map = {label: i for i, label in enumerate(label_list)}
+
+    features = []
+    for (ex_index, example) in enumerate(examples):
+        len_examples = 0
+        if is_tf_dataset:
+            example = processor.get_example_from_tensor_dict(example)
+            example = processor.tfds_map(example)
+            len_examples = tf.data.experimental.cardinality(examples)
+        else:
+            len_examples = len(examples)
+        if ex_index % 10000 == 0:
+            logger.info("Writing example %d/%d" % (ex_index, len_examples))
+
+        inputs = tokenizer.encode_plus(example.text_a, example.text_b, add_special_tokens=True, max_length=max_length,)
+        input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
+
+        # The mask has 1 for real tokens and 0 for padding tokens. Only real
+        # tokens are attended to.
+        attention_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
+
+        # Zero-pad up to the sequence length.
+        padding_length = max_length - len(input_ids)
+        if pad_on_left:
+            input_ids = ([pad_token] * padding_length) + input_ids
+            attention_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + attention_mask
+            token_type_ids = ([pad_token_segment_id] * padding_length) + token_type_ids
+        else:
+            input_ids = input_ids + ([pad_token] * padding_length)
+            attention_mask = attention_mask + ([0 if mask_padding_with_zero else 1] * padding_length)
+            token_type_ids = token_type_ids + ([pad_token_segment_id] * padding_length)
+
+        assert len(input_ids) == max_length, "Error with input length {} vs {}".format(len(input_ids), max_length)
+        assert len(attention_mask) == max_length, "Error with input length {} vs {}".format(
+            len(attention_mask), max_length
+        )
+        assert len(token_type_ids) == max_length, "Error with input length {} vs {}".format(
+            len(token_type_ids), max_length
+        )
+
+        if output_mode == "classification":
+            label = label_map[example.label]
+        elif output_mode == "regression":
+            label = float(example.label)
+        else:
+            raise KeyError(output_mode)
+
+        if ex_index < 5:
+            logger.info("*** Example ***")
+            logger.info("guid: %s" % (example.guid))
+            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+            logger.info("attention_mask: %s" % " ".join([str(x) for x in attention_mask]))
+            logger.info("token_type_ids: %s" % " ".join([str(x) for x in token_type_ids]))
+            logger.info("label: %s (id = %d)" % (example.label, label))
+
+        features.append(
+            InputFeatures(
+                input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, label=label
+            )
+        )
+
+    if is_tf_available and is_tf_dataset:
+
+        def gen():
+            for ex in features:
+                yield (
+                    {
+                        "input_ids": ex.input_ids,
+                        "attention_mask": ex.attention_mask,
+                        "token_type_ids": ex.token_type_ids,
+                    },
+                    ex.label,
+                )
+
+        return tf.data.Dataset.from_generator(
+            gen,
+            ({"input_ids": tf.int32, "attention_mask": tf.int32, "token_type_ids": tf.int32}, tf.int64),
+            (
+                {
+                    "input_ids": tf.TensorShape([None]),
+                    "attention_mask": tf.TensorShape([None]),
+                    "token_type_ids": tf.TensorShape([None]),
+                },
+                tf.TensorShape([]),
+            ),
+        )
+
+    return features
+
+
+
+class imhoProcessor(DataProcessor):
+    """Processor for the IMHO data set."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+        """See base class."""
+        return InputExample(
+            tensor_dict["idx"].numpy(),
+            tensor_dict["sentence1"].numpy().decode("utf-8"),
+            tensor_dict["sentence2"].numpy().decode("utf-8"),
+            str(tensor_dict["label"].numpy()),
+        )
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "imho+context.tsv")))
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "imho+context.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            text_a = line[0]
+            text_b = line[1]
+            if "imho" in text_a or "imo" in text_a: #Gengyu: shoud I remove this two words from data
+                label = "1"
+            else:
+                label = "0"
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+
+
+class cmvProcessor(DataProcessor):
+    """Processor for the IMHO data set."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+        """See base class."""
+        return InputExample(
+            tensor_dict["idx"].numpy(),
+            tensor_dict["sentence"].numpy().decode("utf-8"),
+            None,#tensor_dict["sentence2"].numpy().decode("utf-8"),
+            str(tensor_dict["label"].numpy()),
+        )
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "CMV/change-my-view-modes-master/v2.0")))
+        return self._create_examples("train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples("dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ['disagreement', 'pathos', 'ethos_pathos', 'evaluation_rational', 'evaluation_emotional', 'logos', 'ethos_logos_pathos', 'agreement', 'ethos', 'ethos_logos', 'interpretation', 'logos_pathos']
+
+    def _create_examples(self, set_type):
+        """Creates examples for the training and dev sets."""
+        dirs = ["./CMV/change-my-view-modes-master/v2.0/positive", "./CMV/change-my-view-modes-master/v2.0/negative"]
+        examples1 = self._load_data(dirs[0], set_type)
+        examples2 = self._load_data(dirs[1], set_type)
+        if set_type == "train":
+            return examples1[:int(len(examples1)*0.9)] + examples2[:int(len(examples2)*0.9)]
+        elif set_type == "dev":
+            return examples1[int(len(examples1)*0.9):] + examples2[int(len(examples2)*0.9):]
+
+    def _load_data(self, dir, set_type):
+        examples = []
+        for filename in os.listdir(dir):
+            if filename.endswith(".ann"):
+                lines = self._read_tsv(os.path.join(dir, filename))
+                #print(os.path.join(directory, filename))
+                for (i, line) in enumerate(lines):
+                    #line = line.split("\t")
+                    if line[0].startswith("T"):
+                        guid = "%s-%s" % (set_type, len(examples))
+                        text_a = line[2].replace("CMV:","").strip()
+                    if line[0].startswith("A"):
+                        label = line[1].split(" ")[2].strip()
+                        examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
+
+
+tasks_num_labels = {
+    "imho": 2,
+    "cmv": 12,
+}
+
+processors = {
+    "imho": imhoProcessor,
+    "cmv": cmvProcessor,
+
+}
+
+output_modes = {
+    "imho": "classification",
+    "cmv": "classification",
+
+}
